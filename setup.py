8763a3d63a4cc728e410033c636e8e3f56b8d623
Yenho Chen <yenhochen97@gmail.com>
2023-11-21 14:36:06 -0500

    licensing

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1'
+DESCRIPTION = 'Probabilistic Decomposed Linear Dynamical Systems'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="pdLDS",
+    version=VERSION,
+    author="Yenho Chen",
+    author_email="<yenho@gatech.edu>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    # install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'dynamical systems',],
+    classifiers=[
+        # "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
